fcff2a3dfc6bb8ea2b9ad31973a69169fd3d3f53
azzaz <azzaz@azzazs-Mac.local>
2018-03-13 17:06:13 +0200

    Initial Commit

---
+++ Calc.h
@@ -0,0 +1,13 @@
+//
+//  Calc.h
+//  CalcCat
+//
+//  Created by azzaz on 3/13/18.
+//  Copyright © 2018 azzaz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Calc : NSObject
+
+@end
